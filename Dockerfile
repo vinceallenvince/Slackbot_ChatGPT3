9a3617fcc04e54ce74b8de6911bd24195bbdf4f9
Vince Allen <vince@spotify.com>
2023-02-05 07:31:02 -0500

    Update env vars

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 # Start with python 3.11 image
 FROM python:3.11-slim
 
+# Environment vars
+ARG PROJECT_ID=local
+ENV PROJECT_ID=$PROJECT_ID
+
 # Copy the current directory into /app on the image
 WORKDIR /app
 COPY . /app
--- lib.py
+++ lib.py
@@ -7,11 +7,14 @@ openai.api_key = access_secret_version("OPENAI_API_KEY")
 MODEL = "text-davinci-003"
 
 def access_secret_version(secret_id, version_id="latest"):
+
+	project_id = os.environ["PROJECT_ID"]
+
     # Create the Secret Manager client.
     client = secretmanager.SecretManagerServiceClient()
 
     # Build the resource name of the secret version.
-    name = f"projects/home-375920/secrets/{secret_id}/versions/{version_id}"
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
 
     # Access the secret version.
     response = client.access_secret_version(name=name)
