44181523fe11e472ae9245044a14ab8e60f5b050
Vince Allen <vince@spotify.com>
2023-02-05 07:47:46 -0500

    Move functions out of lib

--- lib.py
+++ lib.py
@@ -1,29 +1,6 @@
 import os
-import openai
 
-from google.cloud import secretmanager
 
-openai.api_key = access_secret_version("OPENAI_API_KEY")
-MODEL = "text-davinci-003"
 
-def access_secret_version(secret_id, version_id="latest"):
 
-	project_id = os.environ["PROJECT_ID"]
 
-    # Create the Secret Manager client.
-    client = secretmanager.SecretManagerServiceClient()
-
-    # Build the resource name of the secret version.
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-
-    # Access the secret version.
-    response = client.access_secret_version(name=name)
-
-    # Return the decoded payload.
-    return response.payload.data.decode('UTF-8')
-
-def create_completion(prompt):
-	completion = openai.Completion.create(
-    	prompt=prompt, model=MODEL, temperature=1, max_tokens=128
-	)
-	return completion
--- main.py
+++ main.py
@@ -3,14 +3,36 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-from lib import create_completion
-from lib import access_secret_version
-
 from google.cloud import logging
 logging_client = logging.Client()
 logger = logging_client.logger("slackbot_gpt_logger")
 logger.log_text("Starting app...")
 
+from google.cloud import secretmanager
+def access_secret_version(secret_id, version_id="latest"):
+	project_id = os.environ["PROJECT_ID"]
+    
+    # Create the Secret Manager client.
+	client = secretmanager.SecretManagerServiceClient()
+	
+    # Build the resource name of the secret version.
+	name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+	response = client.access_secret_version(name=name)
+
+    # Return the decoded payload.
+	return response.payload.data.decode('UTF-8')
+
+import openai
+openai.api_key = access_secret_version("OPENAI_API_KEY")
+MODEL = "text-davinci-003"
+
+def create_completion(prompt):
+	completion = openai.Completion.create(
+    	prompt=prompt, model=MODEL, temperature=1, max_tokens=128
+	)
+	return completion
 
 # Install the Slack app and get xoxb- token in advance from the OAuth & Permissions section of your app's config
 # Bot tokens represent a bot associated with the app installed in a workspace. Unlike user tokens, they're not tied to a user's identity; they're just tied to your app.
